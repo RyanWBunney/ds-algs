04d694901a1700256e2f2ec748e8fa7c4b9c1964
RyanWBunney <rwbunney@gmail.com>
2016-09-24 02:15:25 -0700

    Added src/ directory and some future files

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/* Used to test the algorithms/data-structures written in C
+ * ctests used to test, based off MinUnit; www.jera.com/techinfo/jtns/jtn002.html 
+ * */
+#include <stdio.h> 
+#include <stdlib.h>
+
+
+int main(void)
+{
+    printf("Welcome to Data Structures and Algorithms\n");
+    return 0;
+}
+
